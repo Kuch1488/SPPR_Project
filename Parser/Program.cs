329409b698f739af9e9d9bc4274f2df9eea12c3a
никита беляк <nik.belyak.2001@mail.ru>
2021-12-28 00:14:20 +0300

    added model

--- Parser/Program.cs
+++ Parser/Program.cs
@@ -4,6 +4,7 @@ using Parser.Parserr.Population;
 using System.Text.RegularExpressions;
 
 List<string[]> l = new List<string[]>();
+List<string[]> r = new List<string[]>();
 string[] countries = { "belarus", "bulgaria", "hungary", "moldavia", "poland", "romania", "slovakia", "ukraine", "czech-republic", "turkey", "armenia", "georgia", "azerbaijan" };
 
 ParserWorker<string[]> parser;
@@ -18,6 +19,98 @@ parser.Settings = new ParserSettings(0, 13);
 parser.Start();
 XLSX(l, countries);
 Console.WriteLine();
+
+static void FinalModel(List<string[]> l, List<string[]> r)
+{
+    using (var reader = new StreamReader(@""))
+    {
+        List<string[]> listA = new List<string[]>();
+        List<string> listB = new List<string>();
+        while (!reader.EndOfStream)
+        {
+            var line = reader.ReadLine();
+            var values = line.Split(';');
+
+            r.Add(values);
+        }
+    }
+
+    for (int i = 0; i < r.Count; i++)
+    {
+        string[] t = r[i];
+        t = t.Where(s => s != "").ToArray();
+        r[i] = t;
+    }
+
+    for (int i = 0; i < r.Count - 2; i++)
+    {
+
+    }
+
+    string[] c = r[1];
+    string city = c[2];
+
+    int citizens = 1;
+
+    for (int i = 0; i < l.Count; i++)
+    {
+        string[] s = l[i];
+        for (int j = 0; j < s.Length - 1; j++)
+        {
+            string[] spl = s[j].Split(" ");
+            if (spl.Length == 3 && spl[1] == city)
+                citizens = Convert.ToInt32(spl[2]);
+        }
+
+    }
+
+    // Instantiate a Workbook object that represents Excel file.
+    Workbook wb = new Workbook();
+
+    // When you create a new workbook, a default "Sheet1" is added to the workbook.
+    Worksheet sheet = wb.Worksheets[0];
+
+    // Access the "A1" cell in the sheet.
+    Cell cell = sheet.Cells["A1"];
+
+    // Input the "Hello World!" text into the "A1" cell.
+    cell.PutValue($"{city}");
+
+    cell = sheet.Cells["A2"];
+
+    cell.PutValue("x");
+
+    cell = sheet.Cells["B2"];
+
+    cell.PutValue("y");
+
+    cell = sheet.Cells["C2"];
+
+    cell.PutValue("адрес");
+
+    cell = sheet.Cells["D2"];
+
+    cell.PutValue("жители");
+
+    //Алгоритм для расселения
+    int qwe = citizens / r.Count;
+    for (int i = 1; i < r.Count - 1; i++)
+    {
+        string[] s = r[i];
+        cell = sheet.Cells[$"A{i + 3}"];
+        cell.PutValue($"{s[0]}");
+        cell = sheet.Cells[$"B{i + 3}"];
+        cell.PutValue($"{s[1]}");
+        cell = sheet.Cells[$"C{i + 3}"];
+        cell.PutValue($"{s[3]}");
+        cell = sheet.Cells[$"D{i + 3}"];
+        cell.PutValue($"{qwe}");
+
+    }
+
+    // Save the Excel as .xlsx file.
+    wb.Save($"{city}.xlsx", SaveFormat.Xlsx);
+}
 
 static void XLSX(List<string[]> list, string[] countries)
 {
